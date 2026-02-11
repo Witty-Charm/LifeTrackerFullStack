9e2cc74f4ffdd6a22aac99cd4ef84b3a8be12229
Jolly-Bell <91386898+Jolly-Bell@users.noreply.github.com>
2026-02-11 09:14:44 +0300

    add retrofit responses with sealed class

---
+++ LifeTrackerMobile/app/src/main/java/com/lifetracker/mobile/core/network/ApiError.kt
@@ -0,0 +1,2 @@
+package com.lifetracker.mobile.core.network
+
---
+++ LifeTrackerMobile/app/src/main/java/com/lifetracker/mobile/core/network/NetworkResult.kt
@@ -0,0 +1,7 @@
+package com.lifetracker.mobile.core.network
+
+sealed class NetworkResult<T : Any> {
+    data class Success<T: Any>(val data: T) : NetworkResult<T>()
+    data class Error<T: Any>(val code: Int, val message: String?) : NetworkResult<T>()
+    data class Exception<T: Any>(val e: Throwable) : NetworkResult<T>()
+}
