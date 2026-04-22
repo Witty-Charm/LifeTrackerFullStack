b207263c13ab967959792ecf2adc62608b3e3bfe
Witty-Charm <jollybelll@mail.ru>
2026-04-22 14:31:14 +0300

    chore(migration): add migrations

---
+++ LifeTrackerBackend/Migrations/20260422094237_AddHeroRowVersionConcurrency.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LifeTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHeroRowVersionConcurrency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Heroes",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.Sql("""
+                UPDATE Heroes
+                SET RowVersion = randomblob(8)
+                WHERE RowVersion = x'';
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Heroes");
+        }
+    }
+}
